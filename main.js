455686fc142dd1d6952d20147b77fcdd942c630d
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-10-18 13:49:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function setup() {
+    canvas = createCanvas (400, 400); 
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis;
+    }
+    
+    function clearCanvas() {
+    background("white");
+    }
+    function preload(){
+        classifier= ml5.imageClassifier('DoodleNet');
+    }
+    
+    function draw() {
+        // Set stroke weight to 13
+        strokeWeight(13);
+        // Set stroke color to black
+        stroke(0);
+        // If mouse is pressed, draw line between previous and current mouse positions
+        if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+        }
+    }
+    
+    function classifyCanvas(){
+        classifier.classify(canvas, gotResult);
+    }
+    
+    function gotResult(error, results) {
+        if (error) {
+        console.error(error);
+        }
+        console.log(results);
+        document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+        document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence *100) + '%';
+        utterThis = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
+    function timer_counter(){
+       
+document.getElementById(time).innerHTML= 'Timer: ' + timer_counter; console.log(timer_counter);
+
+if(timer_counter > 400)
+{
+timer_counter = 0;
+
+timer_check="completed";
+}
+ if(timer_check =="completed" || answer_holder == "set")
+{
+timer_check="300";
+answere_holder="";
+updateCanvas();
+}
+    }
